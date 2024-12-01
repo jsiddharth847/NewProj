3defc3534ba47c9a1cd6b7281571bf87e34bc2c8
Siddharth Jain <jsiddharth847@gmail.com>
2024-12-02 00:59:33 +0530

    added validation middleware ,created login route

--- server/routes/auth-router.js
+++ server/routes/auth-router.js
@@ -1,11 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-// const { home } = require('../Controllers/auth-controller'); 
- // Destructure 'home' from the exported object
- const authController = require('../Controllers/auth-controller');
+// const { home } = require('../Controllers/auth-controller');
+// Destructure 'home' from the exported object
+const signupSchema = require("../validator/validator");
+const validate = require("../middlewares/validatorMiddleware");
+const authController = require("../Controllers/auth-controller");
 
-router.route("/").get(authController.home); 
-router.route("/register").post(authController.register);  
+router.route("/").get(authController.home);
+router.route("/register").post(validate(signupSchema), authController.register);
+router.route("/login").post(authController.login);
 
 // router.route("/register").get(register)
 module.exports = router;
