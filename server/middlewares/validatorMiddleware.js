5182288a1ea240654f7d81c2fa6dd1a9786c2cbf
Siddharth Jain <jsiddharth847@gmail.com>
2024-12-02 01:38:35 +0530

    Update validation middleware to include detailed error handling and forward errors to the next middleware

--- server/middlewares/validatorMiddleware.js
+++ server/middlewares/validatorMiddleware.js
@@ -5,9 +5,17 @@ const validate = (schema) => async (req, res, next) => {
     req.body = parsebody;
     next();
   } catch (error) {
-    const errMsg = error.errors[0].message;
-    console.error("Validation error:", errMsg);
-    res.status(400).json({ msg: errMsg });
+    const status = 422;
+    const message = "Invalid request payload";
+    const extradetails = error.errors[0].message;
+    const err = {
+      status,
+      message,
+      extradetails,
+    };
+    console.error("Validation error:", err);
+    // res.status(400).json({ msg: errMsg });
+    next(err);
   }
 };
 
