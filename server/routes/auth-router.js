99a3096e968e7ce1763c6bae879c6348d17fb323
Siddharth Jain <125799102+jsiddharth847@users.noreply.github.com>
2024-12-02 10:42:38 +0530

    Add login validation schema to authentication routes for login and registration.

--- server/routes/auth-router.js
+++ server/routes/auth-router.js
@@ -3,12 +3,13 @@ const router = express.Router();
 // const { home } = require('../Controllers/auth-controller');
 // Destructure 'home' from the exported object
 const signupSchema = require("../validator/validator");
+const loginSchema = require("../validator/validator");
 const validate = require("../middlewares/validatorMiddleware");
 const authController = require("../Controllers/auth-controller");
 
 router.route("/").get(authController.home);
 router.route("/register").post(validate(signupSchema), authController.register);
-router.route("/login").post(authController.login);
+router.route("/login").post(validate(loginSchema),authController.login);
 
 // router.route("/register").get(register)
 module.exports = router;
