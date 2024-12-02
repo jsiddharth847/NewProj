a9bfa2d6bc1f08559654adb521889d0b03590a86
Siddharth Jain <125799102+jsiddharth847@users.noreply.github.com>
2024-12-02 10:36:19 +0530

    Implement contactUs controller to handle contact form submissions. Add validation for required fields and successful form submission response.

---
+++ server/routes/contact-router.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const { contactUsSchema } = require("../validator/validator"); 
+const validate = require("../middlewares/validatorMiddleware");
+const contactController = require("../Controllers/contact-controller");
+
+router.route("/contact")
+  .post(validate(contactUsSchema), contactController.contactUs);
+
+module.exports = router;
