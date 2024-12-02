458c5e7cf4e9e7a88e87a922fef84f011a385d34
Siddharth Jain <125799102+jsiddharth847@users.noreply.github.com>
2024-12-02 10:38:11 +0530

    Refactor validation middleware to handle Zod validation errors. Include error details in the response for better error handling.

--- server/validator/validator.js
+++ server/validator/validator.js
@@ -28,4 +28,50 @@ const signupSchema = z.object({
     .max(255, { message: "Password must be not more than 255 characters" }),
 });
 
-module.exports = signupSchema;
+
+const loginSchema = z.object({
+
+  email: z
+    .string({ required_error: "email is required" })
+    .trim()
+    .min(3, { message: "email must be of 3 chars" })
+    .max(255, { message: "email must be not more than 255 characters" }),
+
+  password: z
+    .string({ required_error: "Password is required" })
+    .trim()
+    .min(8, { message: "Password must be of 3 chars" })
+    .max(255, { message: "Password must be not more than 255 characters" }),
+});
+
+
+const contactUsSchema= z.object({
+  Firstname: z
+    .string({ required_error: "First Name is required" })
+    .trim()
+    .min(3, { message: "First Name must be of 3 chars" })
+    .max(255, { message: "First Name must be not more than 255 characters" }),
+
+    Lastname: z
+    .string({ required_error: "Last Name is required" })
+    .trim()
+    .min(3, { message: "Last Name must be of 3 chars" })
+    .max(255, { message: "Last Name must be not more than 255 characters" }),
+
+  email: z
+    .string({ required_error: "email is required" })
+    .trim()
+    .min(3, { message: "email must be of 3 chars" })
+    .max(255, { message: "email must be not more than 255 characters" }),
+
+  Message: z
+    .string({ required_error: "Phone is required" })
+    .trim()
+    .min(10, { message: "Phone must be of 10 chars" })
+    .max(13, {
+      message: "Phone must be not more than 13 characters including ",
+    }),
+
+});
+
+module.exports = {signupSchema , contactUsSchema , loginSchema};
