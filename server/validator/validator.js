038273844fde7854dbc221aa89e99cc79154d785
Siddharth Jain <jsiddharth847@gmail.com>
2024-12-03 23:39:04 +0530

    corrected the wrong message issues

--- server/validator/validator.js
+++ server/validator/validator.js
@@ -28,9 +28,7 @@ const signupSchema = z.object({
     .max(255, { message: "Password must be not more than 255 characters" }),
 });
 
-
 const loginSchema = z.object({
-
   email: z
     .string({ required_error: "email is required" })
     .trim()
@@ -44,15 +42,14 @@ const loginSchema = z.object({
     .max(255, { message: "Password must be not more than 255 characters" }),
 });
 
-
-const contactUsSchema= z.object({
+const contactUsSchema = z.object({
   Firstname: z
     .string({ required_error: "First Name is required" })
     .trim()
     .min(3, { message: "First Name must be of 3 chars" })
     .max(255, { message: "First Name must be not more than 255 characters" }),
 
-    Lastname: z
+  Lastname: z
     .string({ required_error: "Last Name is required" })
     .trim()
     .min(3, { message: "Last Name must be of 3 chars" })
@@ -65,13 +62,12 @@ const contactUsSchema= z.object({
     .max(255, { message: "email must be not more than 255 characters" }),
 
   Message: z
-    .string({ required_error: "Phone is required" })
+    .string({ required_error: "Message is required" })
     .trim()
-    .min(10, { message: "Phone must be of 10 chars" })
-    .max(13, {
-      message: "Phone must be not more than 13 characters including ",
+    .min(10, { message: "Message must be of 10 chars" })
+    .max(1000, {
+      message: "Message must be not more than 1000 characters including ",
     }),
-
 });
 
-module.exports = {signupSchema , contactUsSchema , loginSchema};
+module.exports = { signupSchema, contactUsSchema, loginSchema };
