ff007d3c36645dfa5dd89421f9f190debd105352
Siddharth Jain <jsiddharth847@gmail.com>
2024-12-03 23:37:55 +0530

    adding destructuring validate

--- server/routes/contact-router.js
+++ server/routes/contact-router.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { contactUsSchema } = require("../validator/validator"); 
-const validate = require("../middlewares/validatorMiddleware");
+const { contactUsSchema } = require("../validator/validator");
+const { validate } = require("../middlewares/validatorMiddleware");
 const contactController = require("../Controllers/contact-controller");
 
-router.route("/contact")
+router
+  .route("/contact")
   .post(validate(contactUsSchema), contactController.contactUs);
 
 module.exports = router;
