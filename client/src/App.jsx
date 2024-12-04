a5de088de5a731fe62e92bb5c4e99651d2378727
Siddharth Jain <jsiddharth847@gmail.com>
2024-12-05 00:00:59 +0530

    u

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
