682df76820b46adbe7aba20a4b0ba874d6adcff3
Siddharth Jain <jsiddharth847@gmail.com>
2024-12-06 23:27:42 +0530

    added imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Services } from "./pages/Services";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
 function App() {
   return (
     <>
